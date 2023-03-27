3891f0a92583b75728556e575b22ff322f04c7cd
Ebibeck <daziebiatri909@gmail.com>
2023-03-27 17:40:09 +0100

    commit

--- my-next-app/app/components/BottomNavBar.jsx
+++ my-next-app/app/components/BottomNavBar.jsx
@@ -1,12 +1,14 @@
 import Navlink from './NavLink';
+import {MdHome, MdMessage} from 'react-icons/md'
+import {TbSettingsFilled} from 'react-icons/tb'
 
 export default function NavlinkBar() {
   return (
     <nav className='flex bg-gray-800 h-full w-full sm:px-2 items-center justify-center'>
       <div className='flex space-x-6'>
-        <Navlink href='/'>Home</Navlink>
-        <Navlink href='#'>Inbox</Navlink>
-        <Navlink href='#'>Settings</Navlink>
+        <Navlink href='/'>{<MdHome/>}Home</Navlink>
+        <Navlink href='#'>{<MdMessage/>}Inbox</Navlink>
+        <Navlink href='#'>{<TbSettingsFilled/>}Settings</Navlink>
       </div>
     </nav>
   );
--- my-next-app/app/components/NavLink.jsx
+++ my-next-app/app/components/NavLink.jsx
@@ -2,15 +2,21 @@
 import Link from "next/link";
 import { useSelectedLayoutSegments } from "next/navigation";
 
-export default function Navlink({ href, children }) {
+export default function Navlink({ href, children}) {
   const segment = useSelectedLayoutSegments();
   const active = href === `/${segment}`;
   return (
     <Link
-      className={active ? "px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700" : "px-3 py-2 rounded-md text-sm font-medium text-gray-100 hover:text-white hover:bg-gray-500 focus:outline-none"}
+      className={
+        active
+          ? "px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+          : "px-3 py-2 rounded-md text-sm font-medium text-gray-100 hover:text-white hover:bg-gray-500 focus:outline-none"
+      }
       href={href}
     >
-      {children}
+      <div className="flex h-full w-full items-center justify-center flex-col">
+        {children}
+      </div>
     </Link>
   );
 }
