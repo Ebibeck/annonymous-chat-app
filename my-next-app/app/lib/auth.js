59daa51aa3fd778bb88153480829bf87534cbea0
Ebibeck <daziebiatri909@gmail.com>
2023-05-04 20:39:38 +0100

    adding more ui components

--- my-next-app/app/lib/auth.js
+++ my-next-app/app/lib/auth.js
@@ -23,7 +23,7 @@ export const AuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
           })
       ],
-      callbacks: {
+      callbacks: { 
         async signIn({ account, profile }) {
           if (account.provider === "google") {
             return profile.email_verified && profile.email.endsWith("@example.com")
