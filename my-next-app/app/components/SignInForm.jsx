aa9d7eb76b12c572ed1c38c1e572403959dab462
Ebibeck <daziebiatri909@gmail.com>
2023-05-07 23:29:28 +0100

    removed the auth layout

---
+++ my-next-app/app/components/SignInForm.jsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { Button } from "../../components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../components/ui/card";
+import { Input } from "../../components/ui/input";
+
+export default function SignInForm() {
+  return (
+    <Card className="flex flex-col items-center w-96 h-auto gap-y-1">
+      <CardHeader className="flex flex-col items-center gap-y-1">
+        <CardTitle className="text-xl text-gray-700 font-semibold">
+          Create an account
+        </CardTitle>
+        <CardDescription className="font-semibold">
+          Enter your email below to create your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="w-full">
+        <form className="flex flex-col w-full items-center gap-y-3">
+          <Input
+            type="email"
+            placeholder="name@gmail.com"
+            className="h-10 font-normal"
+          />
+          <Button className="w-full">Sign In with Email</Button>
+          <p className="text-sm text-muted-foreground">~ OR CONTINUE WITH ~</p>
+          <Button variant="outline" className="w-full font-semibold gap-x-1 mt-3">
+            <Image
+              width={13}
+              height={13}
+              src={'google.svg'}
+              alt=""
+             />
+            <span>Google</span>
+          </Button>
+        </form>
+      </CardContent>
+      <CardFooter>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ my-next-app/app/login/page.jsx
@@ -0,0 +1,8 @@
+import SignInForm from '../components/SignInForm'
+export default function SignIn() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+        <SignInForm />
+    </div>
+  );
+}
